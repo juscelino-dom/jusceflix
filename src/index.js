6510ce7345440c78ffc4cd007e08870b3353c657
Juscelino <juscelino.dom@gmail.com>
2020-08-04 11:55:42 -0300

    desfazendo alteraçoes e comentando as novas

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-  <Switch>
-    <Route path="/" component={App} exact />
-    }
-  </Switch>
-</BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
+//   <BrowserRouter>
+//   <Switch>
+//     <Route path="/" component={App} exact />
+//   </Switch>
+// </BrowserRouter>,
   document.getElementById('root')
 );
